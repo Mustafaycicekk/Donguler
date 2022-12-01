1a8ed747147031cc08806917a49bb657397fa968
Mustafaycicekk <aycicekmustafa77@gmail.com>
2022-12-01 20:18:16 +0300

    Patika.dev Odev

--- YildizlamaElmas/src/main.java
+++ YildizlamaElmas/src/main.java
@@ -7,6 +7,15 @@ public class main {
 		System.out.print("Kaç Basamaklı Olsun: ");
 		int n = scan.nextInt();
 
+		for (int i = 1; i <= n; i++) {
+			for (int k = 1; k <= (n - i); k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= (2 * i) - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		for (int i = n - 1; i >= 1; i--) {
 			for (int k = 1; k <= (n - i); k++) {
 				System.out.print(" ");
@@ -17,4 +26,5 @@ public class main {
 			System.out.println();
 		}
 	}
+
 }
